08d08ed59407225c3d41531cb2547c91b8d5ca6d
Humza Butt <Humzab1711@hotmail.com>
2024-09-26 18:50:39 +0100

    Allowed game to be started with guest namespace

--- src/Views/HomePage/HomePage.xaml.cs
+++ src/Views/HomePage/HomePage.xaml.cs
@@ -1,15 +1,33 @@
 namespace GuessWhoGame.Views
 {
+    [QueryProperty(nameof(Username), "username")]
     public partial class HomePage : ContentPage
     {
+        public string? Username { get; set; }
+
         public HomePage()
         {
             InitializeComponent();
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (!string.IsNullOrEmpty(Username))
+            {
+                WelcomeLabel.Text = $"Welcome, {Username}!";
+            }
+            else
+            {
+                WelcomeLabel.Text = "Welcome!";
+            }
+        }
+
+        // Add this method to handle the "Start Game" button click
         private async void OnStartGameClicked(object sender, EventArgs e)
         {
-            // Navigate to the GamePage
+            // Navigate to GamePage
             await Shell.Current.GoToAsync("GamePage");
         }
     }
